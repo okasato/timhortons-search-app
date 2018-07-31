7addcd4fd323641444047099fbe5dcb439c04eb1
Shingo Konnai <k1ckap0l03@gmail.com>
2018-07-30 17:48:37 -0700

    add .env to write some key and urls

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,18 +1,35 @@
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    database: 'timhortons'
+  developement: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      database: 'timhortons'
+    },
+    port: 5432,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './createTables/'
+    }
   },
-  port: 5432,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  },
-  seeds: {
-    directory: './createTables/'
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || `postgres://${process.env.USER}@127.0.0.1:5432/kovoel`,
+    port: 5432,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './createTables/'
+    }
   }
 };
