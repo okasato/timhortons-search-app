722861fdfbf1f67cd541a600688b3570e58b6de6
Shingo Konnai <k1ckap0l03@gmail.com>
2018-07-31 06:12:32 -0700

    fix db/index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use("*",(req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  res.header("Content-Type", "application/javascript");
   next();
 });
 
