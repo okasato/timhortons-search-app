75651ee92a3934effe2f26c22c59f88d2ded7ece
Shingo Konnai <k1ckap0l03@gmail.com>
2018-07-30 19:07:04 -0700

    fix express.static in server/index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser'); 
 const express = require('express');
 const PORT = process.env.PORT || 1337;
+const path = require('path');
 const app = express();
 
 app.use("*",(req, res, next) => {
@@ -13,7 +14,8 @@ app.use("*",(req, res, next) => {
   next();
 });
 
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/', [
   bodyParser.json(), 
