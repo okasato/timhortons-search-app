c8dd223ee3d6ac8ecca69a45e0d13d1e4499c17e
Shingo Konnai <k1ckap0l03@gmail.com>
2018-07-30 18:25:07 -0700

    fix db/index.js

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,17 @@
 const Knex = require('knex');
 const config = require('./knexfile');
+const environment = process.env.NODE_ENV || 'development';
+const dbConfig = config[environment];
 
-const knex = Knex({
-  client: config.client,
-  port: config.port,
-  connection: {
-    host: config.connection.host,
-    database: config.connection.database
-  }
-});
+const knex = Knex(dbConfig);
+// const knex = Knex({
+//   client: config.client,
+//   port: config.port,
+//   connection: {
+//     host: config.connection.host,
+//     database: config.connection.database
+//   }
+// });
 
 module.exports = {
   shops: require('./shops')(knex),
